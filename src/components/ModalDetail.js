f729d72967366fac6d4b726791d623981661f2ba
Rizqi Akbar Hidayat <rizqiah.rah14@gmail.com>
2023-06-14 21:25:11 +0700

    add modals and fix the code

--- src/components/ModalDetail.js
+++ src/components/ModalDetail.js
@@ -1,31 +1,46 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { data } from "../data/data";
 
 function MyVerticallyCenteredModal(props) {
+  const [dataById, setDataById] = useState([]);
+
+  useEffect(() => {
+    setDataById(data.filter((item) => item.id === props.id));
+  }, [props.id]);
+
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {props.title}
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+    <React.Fragment>
+      {dataById.map((item) => (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Detail Lapangan {item.nama}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>{item.nama}</h4>
+            <p>Kategori: {item.kategori}</p>
+            <p>Lokasi: {item.lokasi}</p>
+            <p>Harga: {item.harga}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={props.onHide}
+              style={{ backgroundColor: "#ff7315" }}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ))}
+    </React.Fragment>
   );
 }
 
--- src/pages/Kelola.js
+++ src/pages/Kelola.js
@@ -1,123 +1,69 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import MyVerticallyCenteredModal from '../components/ModalDetail'
-
+import React, { useState } from "react";
+import MyVerticallyCenteredModal from "../components/ModalDetail";
+import { data } from "../data/data";
 
 function Kelola() {
-    const [modalShow, setModalShow] = React.useState(false);
-    return (
-        <div>
-            <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                title={'modal'}
-            />
-            <div className="container-fluid">
+  const [modalShow, setModalShow] = useState(false);
+  const [id, setId] = useState(0);
 
-                {/* <!-- Page Heading --> */}
-                <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 className="h3 mb-0 text-gray-800">Kelola Lapangan</h1>
-                    <a href="#" className="d-sm-inline-block btn btn-md btn-outline-dark shadow-sm"><i className="fas fa-plus"></i> Tambah Lapangan</a>
-                </div>
-
-                <table className="table table-striped text-center">
-                    <thead>
-                        <tr className="dibooking-orange text-white">
-                            <th scope="col">No.</th>
-                            <th scope="col">Nama Lapangan</th>
-                            <th scope="col">Kategori Lapangan</th>
-                            <th scope="col">Lokasi</th>
-                            <th scope="col">Harga</th>
-                            <th scope="col">Detail</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="text-dark">
-                            <th scope="row">1</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail" onClick={() => setModalShow(true)}>Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">2</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail" onClick={() => setModalShow(true)}>Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">3</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">4</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">5</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">6</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">7</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">8</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">9</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                        <tr className="text-dark">
-                            <th scope="row">10</th>
-                            <td>Lapangan Futsal Jakselmania</td>
-                            <td>Futsal</td>
-                            <td>Jakarta Selatan</td>
-                            <td>Rp 75.000</td>
-                            <td><button className="btn btn-sm text-white button-detail">Detail</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-            </div>
+  return (
+    <div>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        id={id}
+        title={"modal"}
+      />
+      <div className="container-fluid">
+        {/* <!-- Page Heading --> */}
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 className="h3 mb-0 text-gray-800">Kelola Lapangan</h1>
+          <a
+            href="#"
+            className="d-sm-inline-block btn btn-md btn-outline-dark shadow-sm"
+          >
+            <i className="fas fa-plus"></i> Tambah Lapangan
+          </a>
         </div>
-    )
+
+        <table className="table table-striped text-center">
+          <thead>
+            <tr className="dibooking-orange text-white">
+              <th scope="col">No.</th>
+              <th scope="col">Nama Lapangan</th>
+              <th scope="col">Kategori Lapangan</th>
+              <th scope="col">Lokasi</th>
+              <th scope="col">Harga</th>
+              <th scope="col">Detail</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr className="text-dark" key={item.id}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.nama}</td>
+                <td>{item.kategori}</td>
+                <td>{item.kategori}</td>
+                <td>{item.harga}</td>
+                <td>
+                  <button
+                    className="btn btn-sm text-white button-detail"
+                    onClick={() => {
+                      setModalShow(true);
+                      setId(item.id);
+                    }}
+                  >
+                    Detail
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Kelola
+export default Kelola;
